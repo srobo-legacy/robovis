faf8c80cb0110f19012f99ba03102af4e5076fb5
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-07-10 22:06:58 +0100

    Eliminate blob dsp messages, info send by zero copy now

--- dsp_comms.h
+++ dsp_comms.h
@@ -5,11 +5,8 @@
  * to the dsp, which performs image calculations upon it, and then sends back
  * a set of messages with the following commands: */
 
-#define MSG_BLOB_RED		0
-#define MSG_BLOB_BLUE		1
-#define MSG_BLOB_GREEN		2
-#define MSG_NO_MORE_BLOBS	3
-#define MSG_START_PROCESSING	4
+#define MSG_DONE		0
+#define MSG_START_PROCESSING	1
 
 /* Blob colour is reported in which message it is, x1/y1, x2/y2 are packed into
  * arguments 1 and 2 respectively, x in the lower hword, y in upper hword.
