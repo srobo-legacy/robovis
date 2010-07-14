871385670b289123b335c0441ef0727377e43064
Jeremy Morse <jmorse+git@studentrobotics.org>
2010-07-14 22:24:48 +0100

    Every time we complete processing a window, fetch the next.
    Or rather, wait until the dma request pulling it in finishes.

--- visfunc.cpp
+++ visfunc.cpp
@@ -416,6 +416,12 @@ vis_find_blobs_through_scanlines(uint8_t *yuyv, int width, int height,
 		/* Swap sizes too */
 		ospan = span;
 
+		/* If we're done with one dma buffer, get the next */
+		if ((y % NUM_SCANLINES_TO_READ) == 0) {
+			wind_y = 0;
+			yuyv = get_next_buffer(&extmem, y);
+		}
+
 		cache = 0;
 		span = 0;
 
